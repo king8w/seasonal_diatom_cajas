f8ab191448680d0a56822f8aa840313a67a28d31
xbenitogranell <xavier.benito.granell@gmail.com>
2022-06-03 08:36:45 +0200

    update LCBD map

--- scripts/5-map.R
+++ scripts/5-map.R
@@ -15,15 +15,23 @@ library(cividis)
 library(ggsn)
 library(tidyverse)
 library(viridis)
+library(rnaturalearth)
 
-world <- map_data("world")
+world_df <- map_data("world")
+interest <- c("Ecuador")
+ecuador <- world_df %>% filter(str_detect(region, interest))
 
-southamerica <- ggplot() +
-  geom_polygon(data = world, aes(x=long, y = lat, group = group), fill="lightgrey") +
-  theme(legend.position = "right")+
-  coord_map("albers", parameters = c(-100, -100),  ylim=c(-30,15), xlim=c(-82,-50)) +
-  xlab("Longitude") + ylab("Latitude") +
-  theme_bw()
+world <- ne_countries(scale = "medium", returnclass = "sf")
+
+southamerica <- ggplot(data = world) +
+  geom_sf() +
+  geom_polygon(data=ecuador, aes(x=long, y=lat, group=group), fill="blue") +
+  coord_sf(ylim=c(-55,15), xlim=c(-85,-35)) +
+  theme_bw() +
+  theme(axis.title.x=element_blank(),
+        axis.text.x=element_blank(),
+        axis.title.y = element_blank(), axis.ticks.y = element_blank(), axis.text.y=element_blank(),
+        axis.ticks.x=element_blank())
 southamerica
 
 #Plot elevation base map (raster library)
@@ -58,17 +66,25 @@ plot_cajas <- ggplot(data=df_cajas, aes(y=lat, x=long)) +
   coord_equal()
 plot_cajas
   
+# Prepare dataset to plot
+model_LCBD_var_average <- merge(df1, meta, by=0) %>%
+  dplyr::select(-Row.names) %>%
+  select(-c("Month", "SubCuenca", "rock_type", "Vertiente")) %>%
+  select(-c(4,5)) %>%
+  group_by(Lake) %>%
+  summarise(across(everything(), list(mean)))  
+  
 
 ## Cajas map
 plt_cajas <- ggplot(data=df_cajas, aes(y=lat, x=long))+
   geom_raster(aes(fill=Elevation)) +
   scale_fill_cividis()+
-  geom_point(data=model_LCBD_var_average, aes(x=Longitude, y=Latitude, size=LCBD, colour=wetland))+
+  geom_point(data=model_LCBD_var_average, aes(x=Longitude_1, y=Latitude_1, size=LCBD_1, colour=wetland_1))+
   scale_size(range = c(1,5), breaks = c(0.035,0.015), "LCBD") +
-  geom_text_repel(data=model_LCBD_var_average, aes(Longitude, Latitude, label = Lake),col="white") +
+  geom_text_repel(data=model_LCBD_var_average, aes(Longitude_1, Latitude_1, label = Lake),col="white") +
   xlab("Longitude")+
   ylab("Latitude")+
-  labs(colour = "sqrt wetland (%)") +
+  labs(colour = "Wetland (sqrt %)") +
   labs(fill="Elevation (m)") +
   theme_bw() +
   theme(panel.background = element_blank(),
@@ -84,12 +100,12 @@ plt_cajas
 ##
 composite <- ggdraw() +
   draw_plot(plt_cajas) +
-  draw_plot(LCBDwetland, x = 0.095, y = 0.10, width = .3, height = .3)
-  #draw_plot(LCBDrich, x = 0.40, y = 0.10, width = .3, height = .3) 
-
+  #draw_plot(LCBDwetland, x = 0.095, y = 0.10, width = .3, height = .3)
+  draw_plot(LCBDrich, x=0.15, y=0.1, width = .3, height = .3) +
+  draw_plot(southamerica, x=0.64, y=0.75, width = 0.2, height = 0.2)
 composite
 
 #save plot
-ggsave("outputs/LCBDmap_Wetland_richness.png", last_plot(), height = 8, width = 10)
+ggsave("outputs/LCBDmap_Wetland_richness2.png", last_plot(), height = 8, width = 10)
 
 
